99e5023a433eba4ed929b95ffd9a5f5bb1403281
Mark Lopresto <marklopresto@Marks-iMac.local>
2020-05-07 19:54:58 -0400

    create components navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className="site-header">
+      <section className="top-bar">
+        <div className="menu">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Work</li>
+            <li>Resume</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </section>
+    </header>
+  )
+}
+
+export default Navbar
